990e05fe7f24ccdb4aef939df698ac8321215836
Sonu kumar <kumarshanu062@gmail.com>
2021-09-28 08:31:16 +0530

    gitignore

---
+++ Part V/Trie/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std; 
+void solve(){
+}
+int main()
+{
+//freopen("input.txt","r",stdin);
+//freopen("output.txt","w",stdout);
+
+        int tc=1;
+        while(tc--){
+            solve();
+        }
+    return 0;
+}
